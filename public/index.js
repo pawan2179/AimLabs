7cb4b21369f4d67282dacf7052ce90d82e41633a
Pawan Kumar Singh <pawansingh4418@gmail.com>
2023-10-04 00:47:32 +0530

    added crosshair

--- public/index.js
+++ public/index.js
@@ -6,6 +6,18 @@ let spawnInterval = 2000;
 let mainContainer = document.querySelector(".main-content");
 let screenHeight = screen.height;
 let screenWidth = screen.width;
+let eDpi = 1; //0.24;
+
+const handleMouseMovement = (event) => {
+    let x = event.x;
+    let y = event.y;
+
+    // console.log(event);
+
+    let crosshair = document.querySelector(".crosshair");
+    crosshair.setAttribute("left", `${x*eDpi}px`);
+    crosshair.setAttribute("top", `${y*eDpi}px`);
+};
 
 const playGame = () => {
     if(isGameOn) {
@@ -14,17 +26,15 @@ const playGame = () => {
         console.log({randomX, randomY});
         let target= document.createElement("div");
         target.classList.add("target-class");
-        //target.style.top = randomY;
+        target.classList.add("scale-target");
         
         target.style.top = randomY + 'px';
         target.style.left = randomX + 'px';
-        // target.setAttribute("top", `${randomY}`);
-        // target.setAttribute("left", `${randomX}`);
+
         mainContainer.appendChild(target);
         setTimeout(() => {
             target.style.display = "none";
             mainContainer.removeChild(target);
-            if(gameId)    clearInterval(gameId);
         }, 2000);
     }
 };
@@ -34,9 +44,25 @@ let playBtn = document.querySelector("#play-btn");
 playBtn.addEventListener("click", (event) => {
     playBtn.style.display = "none";
     isGameOn = true;
+
+    const promise = mainContainer.requestPointerLock({
+        unadjustedMovement: true
+    });
+
+    promise.then( () => {
+        console.log("Pointer is locked");
+    });
+    promise.catch((error) => {
+        console.log( "Failed to lock screen with error : ", error);
+    })
+
+    document.addEventListener("mousemove", handleMouseMovement);
+
     setTimeout(() => {
         isGameOn = false;
         playBtn.style.display = "block";
+        if(gameId)    clearInterval(gameId);
+        document.exitPointerLock();
     }, 10000);
     gameId = setInterval(() => {
         playGame();
