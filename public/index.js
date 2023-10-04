fee59754f9ffd2616fd7bae72f7d09d23f6dc3a6
Pawan Kumar Singh <pawansingh4418@gmail.com>
2023-10-05 01:06:54 +0530

    fixed crosshair tracking and reduced target spawn area

--- public/index.js
+++ public/index.js
@@ -6,24 +6,38 @@ let spawnInterval = 2000;
 let mainContainer = document.querySelector(".main-content");
 let screenHeight = screen.height;
 let screenWidth = screen.width;
-let eDpi = 1; //0.24;
+let mainContainerWidth = mainContainer.clientWidth;
+let mainContainerHeight = mainContainer.clientHeight;
+
+let minX = Math.floor ((screenWidth - mainContainerWidth) / 2);
+let minY = Math.floor ((screenHeight - mainContainerHeight) / 2);
+let maxX = mainContainerWidth;
+let maxY = mainContainerHeight;
+let eDpi = 1;
+let crossHairPlace = null;
 
 const handleMouseMovement = (event) => {
-    let x = event.x;
-    let y = event.y;
+    if(isGameOn) {
+        let x = event.x;
+        let y = event.y;
 
-    // console.log(event);
-
-    let crosshair = document.querySelector(".crosshair");
-    crosshair.setAttribute("left", `${x*eDpi}px`);
-    crosshair.setAttribute("top", `${y*eDpi}px`);
+        let crosshair = document.querySelector(".crosshair");
+        crosshair.style.left = `${x*eDpi}px`;
+        crosshair.style.top = `${y*eDpi}px`;
+    }
 };
+
+const resetCrosshair = () => {
+    let crosshair = document.querySelector(".crosshair");
+    crosshair.style.left = Math.floor(screenWidth / 2);
+    crosshair.style.top = Math.floor(screenHeight / 2);
+}
 
 const playGame = () => {
     if(isGameOn) {
-        let randomX = Math.floor(Math.random() * screenWidth);
-        let randomY = Math.floor(Math.random() * screenHeight);
-        console.log({randomX, randomY});
+        let randomX = Math.floor(Math.random() * (maxX - minX + 1) + minX);
+        let randomY = Math.floor(Math.random() * (maxY - minY + 1) + minY);
+        // console.log({randomX, randomY});
         let target= document.createElement("div");
         target.classList.add("target-class");
         target.classList.add("scale-target");
@@ -45,24 +59,16 @@ playBtn.addEventListener("click", (event) => {
     playBtn.style.display = "none";
     isGameOn = true;
 
-    const promise = mainContainer.requestPointerLock({
-        unadjustedMovement: true
-    });
-
-    promise.then( () => {
-        console.log("Pointer is locked");
-    });
-    promise.catch((error) => {
-        console.log( "Failed to lock screen with error : ", error);
-    })
-
+    prevX = (Math.floor(screenWidth / 2));
+    prevY = (Math.floor(screenHeight / 2));
     document.addEventListener("mousemove", handleMouseMovement);
 
     setTimeout(() => {
         isGameOn = false;
         playBtn.style.display = "block";
         if(gameId)    clearInterval(gameId);
-        document.exitPointerLock();
+        // document.exitPointerLock();
+        document.removeEventListener("mousemove", resetCrosshair);
     }, 10000);
     gameId = setInterval(() => {
         playGame();
